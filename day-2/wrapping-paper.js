cd0d98cedf8d14877be3db8270af232c626c49ca
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-07-04 15:50:09 +0530

    Refactored: restructured dimensions passed as arguments

--- day-2/wrapping-paper.js
+++ day-2/wrapping-paper.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 
 const toNumber = (digits) => +digits;
+
 const parseDimensions = (rawDimensions) => {
   return rawDimensions.split("\n")
-    .map(dimension => dimension.split("x")
-      .map(toNumber).sort((a, b) => a - b));
+    .map(dimension => dimension.split("x").map(toNumber).sort((a, b) => a - b));
 };
 
 const sum = (a, b) => a + b;
 const area = (l, b) => l * b;
 const volume = (l, b, h) => l * b * h;
 const perimeter = (l, b) => 2 * sum(l, b);
-const surfaceArea = ([l, b, h]) => 2 * (area(l, b) + area(b, h) + area(h, l));
+const surfaceArea = (l, b, h) => 2 * (area(l, b) + area(b, h) + area(h, l));
 
 const totalSurfaceArea = (dimensions) => {
-  return dimensions.map(dimension => {
-    return surfaceArea(dimension) + area(...dimension);
+  return dimensions.map(([l, b, h]) => {
+    return surfaceArea(l, b, h) + area(l, b);
   }).reduce(sum);
 };
 
 const totalRibbonLength = (dimensions) => {
-  return dimensions.map(dimension => {
-    return volume(...dimension) + perimeter(...dimension);
+  return dimensions.map(([l, b, h]) => {
+    return volume(l, b, h) + perimeter(l, b);
   }).reduce(sum);
 };
 
