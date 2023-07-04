0bb9cd146678a88680a36a25a0de2379f08dd124
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-07-04 14:07:32 +0530

    Refactored: wrote one line functions

--- day-1/part-1.js
+++ day-1/part-1.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
 
-const parseInstructions = (rawText) => {
-  return rawText.trim().split("").map(instruction => {
-    if (instruction === '(') return 1;
-    return -1;
-  });
-}
+const toDirection = (instruction) => instruction === "(" ? 1 : -1;
+const sum = (a, b) => a + b;
+
+// const parseInstructions = (rawText) => {
+//   return rawText.trim().split("").map(instruction => {
+//     if (instruction === '(') return 1;
+//     return -1;
+//   });
+// }
+
+// const finalFloor = (instructions) => {
+//   return instructions.reduce((finalPosition, currentPosition) => {
+//     return finalPosition + currentPosition;
+//   }, 0);
+// }
 
 const finalFloor = (instructions) => {
-  return instructions.reduce((finalPosition, currentPosition) => {
-    return finalPosition + currentPosition;
-  }, 0);
+  return instructions.split("").map(toDirection).reduce(sum, 0);
 }
 
 const main = () => {
   const rawText = fs.readFileSync("day-1/input.txt", "utf-8");
-  const instructions = parseInstructions(rawText);
-  console.log(finalFloor(instructions));
+  console.log(finalFloor(rawText));
 }
 
 main();
