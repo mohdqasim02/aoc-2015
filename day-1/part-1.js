a854398540f4bd1d36f938fad4da95c88c3a198d
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-07-10 19:48:06 +0530

    Refactored: directory structure

--- day-1/part-1.js
+++ day-1/part-1.js
@@ -3,26 +3,28 @@ const fs = require("fs");
 const toDirection = (instruction) => instruction === "(" ? 1 : -1;
 const sum = (a, b) => a + b;
 
-// const parseInstructions = (rawText) => {
-//   return rawText.trim().split("").map(instruction => {
-//     if (instruction === '(') return 1;
-//     return -1;
-//   });
-// }
-
-// const finalFloor = (instructions) => {
-//   return instructions.reduce((finalPosition, currentPosition) => {
-//     return finalPosition + currentPosition;
-//   }, 0);
-// }
-
 const finalFloor = (instructions) => {
-  return instructions.split("").map(toDirection).reduce(sum, 0);
+  return instructions.reduce(sum, 0);
+}
+
+const instructionPositionForBasement = (instructions) => {
+  let floor = 0;
+  for (const position in instructions) {
+    floor += instructions[position];
+    if (floor === -1)
+      return +position + 1;
+  }
 }
 
 const main = () => {
-  const rawText = fs.readFileSync("day-1/input.txt", "utf-8");
-  console.log(finalFloor(rawText));
+  const rawText = fs.readFileSync("./input.txt", "utf-8");
+  const directions = rawText.trim().split("").map(toDirection);
+
+  // PART-1
+  console.log("Final floor:", finalFloor(directions));
+
+  // PART-2
+  console.log("Basement index:", instructionPositionForBasement(directions));
 }
 
 main();
