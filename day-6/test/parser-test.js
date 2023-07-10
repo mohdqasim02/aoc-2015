db362aecbecee93f5a5da1541ed0fd44b216a15e
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-07-10 19:23:00 +0530

    Refactored test names

--- day-6/test/parser-test.js
+++ day-6/test/parser-test.js
@@ -4,48 +4,50 @@ const { parseInstruction } = require("../src/parser");
 
 describe("Parser", () => {
   describe("parseInstruction", () => {
-    it("should give toggle, start and end when a raw instruction is given", () => {
-      const rawInstruction = "toggle 461,550 through 564,900";
-      const start = { rowStart: 461, colStart: 550 };
-      const end = { rowEnd: 564, colEnd: 900 };
-      const actual = parseInstruction(rawInstruction);
-      const expected = [["toggle", start, end]];
+    describe("should give command, start and end when a raw instruction is provided", () => {
+      it("toggle as a command when the instruction has toggle", () => {
+        const rawInstruction = "toggle 461,550 through 564,900";
+        const start = { rowStart: 461, colStart: 550 };
+        const end = { rowEnd: 564, colEnd: 900 };
+        const actual = parseInstruction(rawInstruction);
+        const expected = [["toggle", start, end]];
 
-      assert.deepStrictEqual(actual, expected);
-    });
+        assert.deepStrictEqual(actual, expected);
+      });
 
-    it("should give on, start and end when a raw instruction is given", () => {
-      const rawInstruction = "turn on 461,550 through 564,900";
-      const start = { rowStart: 461, colStart: 550 };
-      const end = { rowEnd: 564, colEnd: 900 };
-      const actual = parseInstruction(rawInstruction);
-      const expected = [["on", start, end]];
+      it("on as a command when the instruction has on", () => {
+        const rawInstruction = "turn on 461,550 through 564,900";
+        const start = { rowStart: 461, colStart: 550 };
+        const end = { rowEnd: 564, colEnd: 900 };
+        const actual = parseInstruction(rawInstruction);
+        const expected = [["on", start, end]];
 
-      assert.deepStrictEqual(actual, expected);
-    });
+        assert.deepStrictEqual(actual, expected);
+      });
 
-    it("should give off, start and end when a raw instruction is given", () => {
-      const rawInstruction = "turn off 461,550 through 564,900";
-      const start = { rowStart: 461, colStart: 550 };
-      const end = { rowEnd: 564, colEnd: 900 };
-      const actual = parseInstruction(rawInstruction);
-      const expected = [["off", start, end]];
+      it("off as a command when the instruction has off", () => {
+        const rawInstruction = "turn off 461,550 through 564,900";
+        const start = { rowStart: 461, colStart: 550 };
+        const end = { rowEnd: 564, colEnd: 900 };
+        const actual = parseInstruction(rawInstruction);
+        const expected = [["off", start, end]];
 
-      assert.deepStrictEqual(actual, expected);
-    });
+        assert.deepStrictEqual(actual, expected);
+      });
 
-    it("should give command, start and end when a raw instruction is given", () => {
-      const rawInstructions = "turn on 461,550 through 564,900\nturn off 461,550 through 564,900\ntoggle 461,550 through 564,900";
-      const start = { rowStart: 461, colStart: 550 };
-      const end = { rowEnd: 564, colEnd: 900 };
-      const actual = parseInstruction(rawInstructions);
-      const expected = [
-        ["on", start, end],
-        ["off", start, end],
-        ["toggle", start, end],
-      ];
+      it("list when multiple instructions are given", () => {
+        const rawInstructions = "turn on 461,550 through 564,900\nturn off 461,550 through 564,900\ntoggle 461,550 through 564,900";
+        const start = { rowStart: 461, colStart: 550 };
+        const end = { rowEnd: 564, colEnd: 900 };
+        const actual = parseInstruction(rawInstructions);
+        const expected = [
+          ["on", start, end],
+          ["off", start, end],
+          ["toggle", start, end],
+        ];
 
-      assert.deepStrictEqual(actual, expected);
+        assert.deepStrictEqual(actual, expected);
+      });
     });
   });
 });
