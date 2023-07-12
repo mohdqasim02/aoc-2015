616c975b7e005f1e8c483409c0ae4f90ba7465bc
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-07-12 18:19:15 +0530

    Integrate: count

--- day-8/main.js
+++ day-8/main.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { countLiteralCharacters, countInMemoryCharacters } = require("./src/matchsticks");
+const { countLiteralCharacters, countInMemoryCharacters, countEncodedCharacters } = require("./src/matchsticks");
 
 const readFile = (filePath) => fs.readFileSync(filePath, "utf-8");
 
@@ -7,8 +7,13 @@ const main = () => {
   const strings = readFile("./resources/input.txt").split("\n");
   const literalLength = countLiteralCharacters(strings);
   const inMemoryLength = countInMemoryCharacters(strings);
+  const encodedLength = countEncodedCharacters(strings);
 
+  // PART-1
   console.log("Difference between literal length and in-memory length is:", literalLength - inMemoryLength);
+
+  // PART-2
+  console.log("Difference between literal length and encoded length is:", encodedLength - literalLength);
 }
 
 main();
